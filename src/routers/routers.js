c904292f47f0bf3ccdf3621cf241e4f5cc368d9f
ANNMAINAWANGARI <wangariann371@gmail.com>
2023-09-11 00:27:26 +0300

    changed route file syntax

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getData } = require("../controllers/controller");
 const router = express.Router();
 
-router.get("/", getData);
+router.get("/api", getData);
 
 module.exports = router;
